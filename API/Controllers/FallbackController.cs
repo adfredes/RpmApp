95ee15904fed742a8a5c5a35d23b0ff474669978
Alejandro Fredes <adfredes@gmail.com>
2020-11-11 11:54:36 -0300

    Fix Fallback

--- API/Controllers/FallbackController.cs
+++ API/Controllers/FallbackController.cs
@@ -2,7 +2,7 @@ using System.IO;
 using Microsoft.AspNetCore.Mvc;
 namespace API.Controllers
 {
-    public class FallbackController
+    public class FallbackController : Controller
     {
         public ActionResult Index ()
         {
